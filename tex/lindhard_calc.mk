636d2a979114a1cdc025b34ea353ec81416f0478
Deepak <dmallubhotla+github@gmail.com>
2020-03-17 20:52:52 -0500

    Adding lindhard_calc

---
+++ tex/lindhard_calc.mk
@@ -0,0 +1,17 @@
+USED_FIG_NAMES_LINDC = chiZELindhard.jpg T1ZELindhard.jpg
+USED_FIGS_LINDC = $(addprefix $(FIG_DIR)/, $(USED_FIG_NAMES_LINDC))
+USED_CALC_NAMES_LINDC = $(USED_FIG_NAMES_LINDC:.jpg=.csv)
+USED_CALC_LINDC = $(addprefix $(CALC_DIR)/, $(USED_CALC_NAMES_LINDC))
+
+.SECONDARY: $(USED_CALC_LINDC)
+
+$(CALC_DIR)/%.csv: $(SCRIPT_DIR)/%Calc.wls | $(CALC_DIR)
+	$(WS) $<
+
+$(FIG_DIR)/%.jpg: $(SCRIPT_DIR)/%Plot.wls $(CALC_DIR)/%.csv | $(FIG_DIR)
+	$(WS) $<
+
+lindhard_calc.pdf: lindhard_calc.tex $(USED_FIGS_LINDC)
+
+MAIN_PDF_DEPS := $(MAIN_PDF_DEPS) $(TEX_DIR)/lindhard_calc.tex $(USED_FIGS_LINDC)
+ALL_PDFS := $(ALL_PDFS) $(PDF_DIR)/lindhard_calc.pdf
