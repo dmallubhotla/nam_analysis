946a820a4b397d6ba4f6a9b8d399def8a61cd334
Deepak <dmallubhotla+github@gmail.com>
2020-03-17 20:17:26 -0500

    Getting ine in and fixincg some minor things

---
+++ tex/interpolated_noise_estimation.mk
@@ -0,0 +1,9 @@
+USED_FIG_NAMES_INE = chiZEarounduFVaryingFrequency.jpg chiZEarounduFVaryingTemp.jpg chiZEarounduFVaryingTau.jpg chiZEarounduFVaryingVf.jpg
+USED_FIGS_INE = $(addprefix $(FIG_DIR)/, $(USED_FIG_NAMES_INE))
+figures/%.jpg: $(SCRIPT_DIR)/%.wls | $(FIG_DIR)
+	$(WS) $<
+
+$(PDF_DIR)/interpolated_noise_estimation.pdf: $(TEX_DIR)/interpolated_noise_estimation.tex $(USED_FIG_INE)
+
+MAIN_PDF_DEPS := $(MAIN_PDF_DEPS) $(TEX_DIR)/interpolated_noise_estimation.tex $(USED_FIGS_INE)
+ALL_PDFS := $(ALL_PDFS) $(PDF_DIR)/interpolated_noise_estimation.pdf
