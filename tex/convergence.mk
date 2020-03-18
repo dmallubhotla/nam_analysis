801b70ed711346838d3b6353aca4333902648443
Deepak <dmallubhotla+github@gmail.com>
2020-03-17 19:29:05 -0500

    Adding title to convergence and fixing mk typo

--- tex/convergence.mk
+++ tex/convergence.mk
@@ -1,4 +1,4 @@
-$(PDF_DIR)/convergence.pdf: $(TEX_DIR)/conductivity_notes.tex
+$(PDF_DIR)/convergence.pdf: $(TEX_DIR)/convergence.tex
 
 MAIN_PDF_DEPS := $(MAIN_PDF_DEPS) $(TEX_DIR)/convergence.tex
 ALL_PDFS := $(ALL_PDFS) $(PDF_DIR)/convergence.pdf
