adb806f57dcc2dd9b030e5ee8294753e65f215f0
Deepak <dmallubhotla+github@gmail.com>
2020-03-23 15:20:41 -0500

    Adding overall method summary tex file also

---
+++ tex/overall_method_summary.mk
@@ -0,0 +1,6 @@
+$(PDF_DIR)/overall_method_summary.pdf: $(TEX_DIR)/overall_method_summary.tex
+
+MAIN_PDF_DEPS := $(MAIN_PDF_DEPS) $(TEX_DIR)/overall_method_summary.tex
+ALL_PDFS := $(ALL_PDFS) $(PDF_DIR)/overall_method_summary.pdf
+
+
